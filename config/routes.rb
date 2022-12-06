873850ee69a0985d8044288263396beaccf7246d
yoshidablue <sugar.blue.1224@gmail.com>
2022-12-06 05:36:55 +0000

    bookers2-debug 基本形作成 完成

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,20 @@ Rails.application.routes.draw do
   root :to =>"homes#top"
   get "home/about"=>"homes#about"
 
-  resources :books, only: [:index,:show,:edit,:create,:destroy,:update]
-  resources :users, only: [:index,:show,:edit,:update]
+  resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
+    # resourcesだといいね取り消しの時ルーティングエラーになる。idが付与されるため。
+    # resourceだとidが付与されない。
+    resource  :favorites,     only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
+  end
+
+  resources :users, only: [:index,:show,:edit,:update] do
+    resource :relationships, only: [:create, :destroy]
+    get "followings" => "relationships#followings", as: "followings"
+    get "followers"  => "relationships#followers",  as: "followers"
+  end
+
+  get "search" => "searches#search"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
